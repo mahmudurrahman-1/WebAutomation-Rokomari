0805b65122130f23095f67d890d5b5cd125bb108
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-06 21:27:28 +0600

    Page Driver methods added

---
+++ src/main/java/drivers/PageDriver.java
@@ -0,0 +1,32 @@
+package drivers;
+
+import org.openqa.selenium.WebDriver;
+
+public class PageDriver {
+    private static final ThreadLocal<WebDriver> webdriver = new ThreadLocal<>();
+    private static PageDriver instance =null;
+    private PageDriver(){
+        //empty
+    }
+    //create instance
+    public static PageDriver getInstance(){
+        if(instance==null){
+            instance=new PageDriver();
+        }
+        return instance;
+    }
+
+    //set driver
+    public void setWebdriver(WebDriver driver){
+        webdriver.set(driver);
+    }
+    //get webdriver
+    public WebDriver getDriver(){
+        return webdriver.get();
+    }
+
+    // Get current driver
+    public static WebDriver getCurrentDriver(){
+        return getInstance().getDriver();
+    }
+}
