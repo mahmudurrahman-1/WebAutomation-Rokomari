aaaa191e08ef6c6c37be17a85ac21e205d8c096d
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-07 15:47:49 +0600

    driver runner created

---
+++ src/test/java/step_def/DriverRunner.java
@@ -0,0 +1,12 @@
+package step_def;
+
+import drivers.MainDriver;
+import io.cucumber.java.Before;
+
+public class DriverRunner extends MainDriver {
+
+    @Before
+    public void startsBrowser(){
+
+    }
+}
