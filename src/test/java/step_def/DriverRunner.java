bf889452e3b3eb4d265647d7069f1f6310ead0ad
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-07 20:36:27 +0600

    created

--- src/test/java/step_def/DriverRunner.java
+++ src/test/java/step_def/DriverRunner.java
@@ -11,15 +11,16 @@ import java.util.Properties;
 public class DriverRunner extends MainDriver {
 
     Properties prop = DataParser.loadProperties("driver.properties");
+
     //This will be started before all the tests
     @Before
-    public void startsBrowser(){
-      driver = setup(prop.getProperty("br1"));
+    public void startsBrowser() {
+        driver = setup(prop.getProperty("br1"));
     }
 
     // After all the test browser will be closed here
     @After
-    public void closeBrowser(){
+    public void closeBrowser() {
         PageDriver.getCurrentDriver().close();
     }
 }
