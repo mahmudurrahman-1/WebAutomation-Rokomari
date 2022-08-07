37e71ed4cd5d08c095fe5d4a9f8fb98b62ae4f55
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-07 16:31:34 +0600

    methods inserted

--- src/test/java/step_def/DriverRunner.java
+++ src/test/java/step_def/DriverRunner.java
@@ -1,12 +1,25 @@
 package step_def;
 
 import drivers.MainDriver;
+import drivers.PageDriver;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import utilities.DataParser;
+
+import java.util.Properties;
 
 public class DriverRunner extends MainDriver {
 
+    Properties prop = DataParser.loadProperties("driver.properties");
+    //This will be started before all the tests
     @Before
     public void startsBrowser(){
+      driver = setup(prop.getProperty("br1"));
+    }
 
+    // After all the test browser will be closed here
+    @After
+    public void closeBrowser(){
+        PageDriver.getCurrentDriver().close();
     }
 }
