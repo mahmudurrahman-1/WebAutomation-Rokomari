6c6aede9ef039a532921778fe6f19f538c5dd228
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-07 19:04:04 +0600

    pending commit

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/java/features",
+        features = "src/test/java/features/registration.feature",
         glue = "step_def"
 )
 public class TestRunner  extends AbstractTestNGCucumberTests {
