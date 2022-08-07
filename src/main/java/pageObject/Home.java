ae43de29c68a8b3693b79f61217d4332d17c9173
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-07 16:31:48 +0600

    home added

---
+++ src/main/java/pageObject/Home.java
@@ -0,0 +1,32 @@
+package pageObject;
+
+import drivers.PageDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Home {
+    public Home(){
+        PageFactory.initElements(PageDriver.getCurrentDriver(),this);
+    }
+
+    /****************
+     * Identifiers
+     */
+    @FindBy(xpath="//a[normalize-space()='Sign in']")
+    public WebElement signinbtn;
+    @FindBy(xpath="//p[normalize-space()='Sign up']")
+    public WebElement signupbtn;
+
+    /****************
+     * Actions
+     */
+
+    public void ClickSignin(){
+        signinbtn.click();
+    }
+    public void ClickSignup(){
+        signupbtn.click();
+    }
+}
