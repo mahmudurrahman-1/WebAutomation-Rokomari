9d9eb7342ff4ae42c549cc2e94f6add5c524a0d2
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-07 20:37:04 +0600

    f-commit

--- src/main/java/drivers/PageDriver.java
+++ src/main/java/drivers/PageDriver.java
@@ -4,29 +4,32 @@ import org.openqa.selenium.WebDriver;
 
 public class PageDriver {
     private static final ThreadLocal<WebDriver> webdriver = new ThreadLocal<>();
-    private static PageDriver instance =null;
-    private PageDriver(){
+    private static PageDriver instance = null;
+
+    private PageDriver() {
         //empty
     }
+
     //create instance
-    public static PageDriver getInstance(){
-        if(instance==null){
-            instance=new PageDriver();
+    public static PageDriver getInstance() {
+        if (instance == null) {
+            instance = new PageDriver();
         }
         return instance;
     }
 
     //set driver
-    public void setWebdriver(WebDriver driver){
+    public void setWebdriver(WebDriver driver) {
         webdriver.set(driver);
     }
+
     //get webdriver
-    public WebDriver getDriver(){
+    public WebDriver getDriver() {
         return webdriver.get();
     }
 
     // Get current driver
-    public static WebDriver getCurrentDriver(){
+    public static WebDriver getCurrentDriver() {
         return getInstance().getDriver();
     }
 }
