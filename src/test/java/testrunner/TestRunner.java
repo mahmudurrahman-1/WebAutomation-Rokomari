c01c67f12d8b817e2f6b036e3d34fe0b0ce1af82
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-08 17:30:19 +0600

    f-commit

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -4,11 +4,13 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-//        features = "src/test/java/features/registration.feature",
+//       features = "src/test/java/features/registration.feature",
 //        features = "src/test/java/features/login.feature",
         features = "src/test/java/features/reset.feature",
 
-        glue = "step_def"
+        glue = "step_def",
+        plugin={"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "timeline:test-output-thread/", "rerun:target/failedrerun.txt"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
